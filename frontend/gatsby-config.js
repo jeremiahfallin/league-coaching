8ae2dac5fefa760734b39cbbd0d60ff30c2ad20b
Tacit <jeremiah.fallin@gmail.com>
2019-08-30 12:52:37 -0700

    Solid point.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,8 +1,10 @@
+"use strict"
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `League Coaching`,
+    description: `Track players in your leagues and keep track of their stats.`,
+    author: `@jeremiahfallin`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -28,6 +30,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-netlify-cms",
+      options: {
+        modulePath: `${__dirname}/src/cms/index.js`,
+      },
+    },
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
