e12051dd5e18a56cb157c93feda75075293c1bdd
Tacit <jeremiah.fallin@gmail.com>
2019-09-05 13:24:03 -0700

    Kind of working.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `https://league-coaching.herokuapp.com/`
+export const endpoint = `https://league-coaching-prod.herokuapp.com/`
