3cd2f43e71345f4461b19f94d46084fb2addd450
Tacit <jeremiah.fallin@gmail.com>
2019-09-26 00:12:03 -0700

    Working.

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,20 +1,29 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from "react";
+import { Link } from "gatsby";
 
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Image from "../components/image";
+import SEO from "../components/seo";
 
 const IndexPage = () => (
   <>
     <SEO title="Home" />
     <p>
-      A tool for recording and tracking stats of teams and players in amateur
-      LoL leagues.
+      This website is a tool for recording and tracking stats of teams and
+      players in amateur League of Legends tournaments/leagues.
     </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
+    <p>
+      At the top, you can add a match with the Match ID of the game that was
+      played. Using that will fill in all of the stats for the game. Afterward,
+      you can either select a team name if they've already played matches that
+      have been submitted, or you can manually type in and add a new one.
+      Lastly, you'll fill out the players who were involved in the game.
+    </p>
+    <p>
+      Clicking View Teams will allow you to look through the stats for the
+      different teams who have had games stored here and also the players who
+      have been on those teams.
+    </p>
   </>
-)
+);
 
-export default IndexPage
+export default IndexPage;
